42a3e5b73861b72fe9d8afc740f3f615fde3e70e
henryflia <superflia2059@gmail.com>
2023-01-07 18:13:12 -0400

    Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        string letter = "A";
+
+        if (number >= 90)
+        {
+            Console.WriteLine(letter);
+        }
+        else if (number >= 80 && number < 90)
+        {
+            letter = "B";
+            Console.WriteLine(letter);
+        }
+        else if (number >= 70 && number < 80)
+        {
+            letter = "C";
+            Console.WriteLine(letter);
+        } 
+        else if (number >= 60 && number < 70)
+        {
+            letter = "D";
+            Console.WriteLine(letter);
+        } 
+        else 
+        {
+            letter = "F";
+            Console.WriteLine(letter);
+        } 
+        if (number >=70)
+        {
+            Console.Write("Congratulation you passed the course");
+        }
+        else 
+            Console.Write("Sorry, i know yoou can do it better the next time");
+
     }
 }
