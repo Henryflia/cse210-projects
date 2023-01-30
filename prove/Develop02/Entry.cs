f11cbc4c920b022489b2c550c943bd5e926501a9
henryflia <superflia2059@gmail.com>
2023-01-30 13:23:46 -0400

    new entry

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -3,6 +3,8 @@ public class Entry
     public List<string> _entrys = new List<string>();
     public void DisplayPrompt()
     {
+        Console.WriteLine("What is your mood today?");
+        string mood = Console.ReadLine();
         Promp_Developer prompte = new Promp_Developer();
         prompte.ListPrompt();
         string x = prompte.RandomPrompt();
@@ -10,6 +12,7 @@ public class Entry
         DateTime theCurrentTime = DateTime.Now;
         string dateText = theCurrentTime.ToShortDateString();
         _entrys.Add(dateText);
+        _entrys.Add(mood);
         _entrys.Add(x);
        
   
