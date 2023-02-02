c44829b5372b82662f35916fdd10a4afa5abcecc
henryflia <superflia2059@gmail.com>
2023-02-01 22:33:25 -0400

    lessson3

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,46 @@
+public class Fraction
+{
+    private float _top;
+    private float _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 5;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    public float GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public float GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public void GetFractionString()
+    {
+        Console.WriteLine($"{_top}/{_bottom}");
+    }
+    public float GetDecimalValue()
+    {
+        return _top/_bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        f1.GetFractionString();
+        f1.GetDecimalValue();
+        Console.WriteLine(f1.GetDecimalValue());
+        Fraction f2 = new Fraction(6);
+        f2.GetFractionString();
+        f2.GetDecimalValue();
+        Console.WriteLine(f2.GetDecimalValue());
+        Fraction f3 = new Fraction(6,7);
+        f3.GetFractionString();
+        f3.GetDecimalValue();
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }
