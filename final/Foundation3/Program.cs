5314a6c1c24eb9bc63071eaf5814d1c06071f670
henryflia <superflia2059@gmail.com>
2023-04-02 00:34:55 -0400

    sss

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,17 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Address lecturea = new Address("Calle Silencio, QueNooo, HayDios");
+        Address lecturea = new Address("Street 5, HIGUEY, c Canada");
         Lectures lecture = new Lectures("Ester","11","Mañana", "Pantalosnes", "3/30/2021", "12:00", lecturea,"Lectures", "Standard");
         Console.WriteLine("-----------------------");
         lecture.DisplaytheEvent();
         Console.WriteLine("-----------------------");
-        Address receptiona = new Address("Calle Silencio, QueNooo, HayDios");
+        Address receptiona = new Address("Street Home, Finish, All");
         Receptions reception = new Receptions("event@gmail.com","Mañana", "Pantalosnes", "3/30/2021", "12:00", receptiona,"Reception Event", "Full");
         Console.WriteLine("-----------------------");
         reception.DisplaytheEvent();
         Console.WriteLine("-----------------------");
-        Address outdoora = new Address("Calle Silencio, QueNooo, HayDios");
+        Address outdoora = new Address("Street Silence, No, Anything");
         Outdoor outdoor = new Outdoor(35,"Mañana", "Pantalosnes", "3/30/2021", "12:00", outdoora,"Outdoor Event", "Short");
         Console.WriteLine("-----------------------");
         outdoor.DisplaytheEvent();
